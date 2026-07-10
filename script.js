89f3de5f28d01e428ea9809b77c9578501a6e2d0
Conexión <studiosconexion@gmail.com>
2026-07-10 02:34:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ function createParticles() {
 
 // Datos
 const musicaData = [
-    { titulo: 'El Legado De Ayer', img: '/Imagenes/Albums/El Legado De Ayer.jpg', desc: 'Álbum 2025 • Edición Banda', url: 'https://push.fm/fl/eyyhwzpr' },
-    { titulo: 'Mi Único Anheló ', img: '/Imagenes/Albums/Mi Unico Anhelo.jpg', desc: 'Álbum 2025 • ', url: '' },
-    { titulo: 'Una Corona', img: '/Imagenes/Albums/Una Corona.jpeg', desc: 'Álbum 2025 • Instrumental', url: 'https://push.fm/fl/bbp96ewk' },
-    { titulo: 'Mientras Dormía', img: '/Imagenes/Albums/Mientras Dormia.jpg', desc: 'Single 2025 • Pop Romántico', url: 'https://youtu.be/fJcODM1mfnw?si=NhgAHjUyYkxiO6Pc' }
+    { titulo: 'El Legado De Ayer', img: 'Imagenes/Portada De Album/El Legado De Ayer.jpg', desc: 'Álbum 2025 • Edición Banda', url: 'https://push.fm/fl/eyyhwzpr' },
+    { titulo: 'Mi Único Anheló ', img: 'Imagenes/Portada De Album/Mi Unico Anhelo.jpg', desc: 'Álbum 2025 • ', url: '' },
+    { titulo: 'Una Corona', img: 'Imagenes/Portada De Album/Una Corona.jpeg', desc: 'Álbum 2025 • Instrumental', url: 'https://push.fm/fl/bbp96ewk' },
+    { titulo: 'Mientras Dormía', img: 'Imagenes/Portada De Album/Mientras Dormia.jpg', desc: 'Single 2025 • Pop Romántico', url: 'https://youtu.be/fJcODM1mfnw?si=NhgAHjUyYkxiO6Pc' }
 ];
 
 const miniseriesData = [
-    { titulo: 'Las Aventuras De Barnaby', img: '', desc: 'Temporada 1 • Piloto • Cristian R.M', url: '' }
+    { titulo: 'Las Aventuras De Barnaby', img: 'Imagenes/Portada De Series/Próximamente.jpg', desc: 'Temporada 1 • Piloto • Cristian R.M', url: '' }
 ];
 
 const eventosData = [
@@ -37,22 +37,21 @@ const eventosData = [
 ];
 
 const redesData = [
-    { nombre: 'Instagram', url: '', icon: '<span class="social-icon instagram"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5Zm0 2a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3H7Zm5 3.5A4.5 4.5 0 1 1 7.5 12 4.5 4.5 0 0 1 12 7.5Zm0 2A2.5 2.5 0 1 0 14.5 12 2.5 2.5 0 0 0 12 9.5Zm5.25-3.25a1.25 1.25 0 1 1-1.25 1.25 1.25 1.25 0 0 1 1.25-1.25Z"/></svg></span>', color: '#E1306C' },
-    { nombre: 'X / Twitter', url: '', icon: '<span class="social-icon x"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M18.9 2H22l-6.8 7.8L23.5 22h-5.5l-4.3-5.6L8.3 22H5.2l7.2-8.2L.5 2h5.6l3.9 5.2L18.9 2Zm-1 18h1.1L6.2 4H5.1l12.8 16Z"/></svg></span>', color: '#1DA1F2' },
-    { nombre: 'YouTube', url: '', icon: '<span class="social-icon youtube"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M23 7.5a3 3 0 0 0-2.1-2.1C19 5 12 5 12 5s-7 0-8.9.4A3 3 0 0 0 1 7.5 31.4 31.4 0 0 0 .5 12a31.4 31.4 0 0 0 .5 4.5 3 3 0 0 0 2.1 2.1C5 19 12 19 12 19s7 0 8.9-.4a3 3 0 0 0 2.1-2.1A31.4 31.4 0 0 0 23.5 12 31.4 31.4 0 0 0 23 7.5Zm-13 8.3V8.2l6 3.8-6 4.8Z"/></svg></span>', color: '#FF0000' },
-    { nombre: 'TikTok', url: '', icon: '<span class="social-icon tiktok"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M14.5 2h2.2a4.8 4.8 0 0 0 4.8 4.8v2.4a7.2 7.2 0 0 1-4.8-1.6v7.1a5.2 5.2 0 1 1-4.9-5.2c.2 0 .4 0 .6.1v2.3a2.9 2.9 0 1 0 2.3 2.8V2Z"/></svg></span>', color: '#000000' },
-    { nombre: 'Spotify', url: '', icon: '<span class="social-icon spotify"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2Zm4.7 14.2a.8.8 0 0 1-1.1.3 11.5 11.5 0 0 0-5.2-1.5 11 11 0 0 0-2.5.3.8.8 0 1 1-.3-1.6 13.7 13.7 0 0 1 3 .4 13.4 13.4 0 0 1 6.2 1.9.8.8 0 0 1 .3 1.1Zm1.2-2.7a1 1 0 0 1-1.3.4 15.2 15.2 0 0 0-6.4-1.8 15.8 15.8 0 0 0-3 .3 1 1 0 0 1-.4-1.9 18.4 18.4 0 0 1 3.5-.4 18.2 18.2 0 0 1 7.5 2.2 1 1 0 0 1 .4 1.3Zm.1-2.8a1.2 1.2 0 0 1-1.5.4 19.8 19.8 0 0 0-7.4-2 20.2 20.2 0 0 0-3.8.4 1.2 1.2 0 0 1-.6-2.3 22.8 22.8 0 0 1 4.5-.4 22.8 22.8 0 0 1 8.6 2.2 1.2 1.2 0 0 1 .4 1.5Z"/></svg></span>', color: '#1DB954' },
-    { nombre: 'Threads', url: '', icon: '<span class="social-icon threads"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2c2.4 0 4.2 1 5.5 2.8 1.2 1.6 1.8 3.5 1.8 5.7a10.7 10.7 0 0 1-2.2 6.5c-.8 1.1-1.9 2.1-3.2 2.9a8.6 8.6 0 0 1-7.4.2 7.3 7.3 0 0 1-3.2-3 8.7 8.7 0 0 1-1-4.3c0-2.4.9-4.6 2.6-6.2C7.2 3.3 9.3 2.4 12 2Zm0 2.2c-1.8 0-3.4.8-4.6 2.2-1.1 1.3-1.7 2.9-1.7 4.6 0 1.5.5 2.9 1.4 4 1 .1 1.9.1 2.8-.1 1.4-.3 2.6-1 3.5-2 .8-1 .9-2.2.9-3.4 0-.6-.1-1.2-.2-1.8-.2-.7-.4-1.3-.8-1.8-.7-.9-1.7-1.4-2.8-1.6-.7-.1-1.3-.1-2 .1a4.2 4.2 0 0 0-1.7 1.2c-.4.5-.7 1-.7 1.6 0 .6.2 1.2.5 1.7a3 3 0 0 0 2.1 1.3c.8.1 1.6-.1 2.3-.4.4-.2.8-.4 1.1-.8.2-.3.3-.6.3-.9 0-.4-.2-.8-.6-1.1-.6-.4-1.4-.5-2.1-.4-.6.1-1.2.4-1.7.8-.4.4-.7.8-.8 1.3-.2.9.1 1.8.8 2.4.6.5 1.4.8 2.2.8.8 0 1.6-.3 2.2-.8.8-.7 1.2-1.7 1.3-2.8.1-1.4-.2-2.8-.9-4.1-.6-1-1.4-1.7-2.4-2.3-.6-.3-1.3-.5-2-.6-.9-.1-1.8-.1-2.6.1-1.3.3-2.4 1-3.2 2-.5.6-.9 1.3-1 2-.2 1-.1 2 .3 2.9.7 1.5 2 2.5 3.6 2.8.9.2 1.8.2 2.8 0 1.4-.3 2.7-1.2 3.6-2.4.8-1.2 1.1-2.6.9-4-.2-1.4-.6-2.8-1.5-3.9C15 4 13.7 2.8 12 2.2Z"/></svg></span>', color: '#000000' },
-    { nombre: 'Facebook', url: '', icon: '<span class="social-icon facebook"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M13.5 22v-8.8h2.9l.4-3.4h-3.3V4.6c0-1 .3-1.7 1.7-1.7h1.8V.1c-.3 0-1.4-.1-2.7-.1-2.7 0-4.5 1.6-4.5 4.7v2.6H7.1v3.4h2.8V22h3.6Z"/></svg></span>', color: '#1877F2' },
-    { nombre: 'WhatsApp', url: '', icon: '<span class="social-icon whatsapp"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M19.1 4.9A9.7 9.7 0 0 0 4.8 19l-1 3.7 3.8-1a9.7 9.7 0 0 0 11.5-15.8Zm-7.2 15a8 8 0 0 1-4.1-1.1l-.3-.2-2.3.6.6-2.2-.2-.3A8 8 0 1 1 11.9 19.9Zm4.5-6c-.2-.1-1.3-.6-1.5-.7s-.4-.1-.5.1-.6.7-.7.8-.3.2-.5.1a6.2 6.2 0 0 1-1.8-1.1 6.8 6.8 0 0 1-1.3-1.6c-.1-.2 0-.3.1-.4l.4-.4c.1-.1.2-.2.3-.3a.4.4 0 0 0 0-.4l-.2-.4a.9.9 0 0 0-.2-.2c-.2-.1-.4-.1-.5-.1h-.4a1 1 0 0 0-.8.4 3.2 3.2 0 0 0-1 2.2 5.6 5.6 0 0 0 1.2 3 5.2 5.2 0 0 0 3.8 2.8 3.1 3.1 0 0 0 1.8.1 2.3 2.3 0 0 0 1.7-1.1.2-.3.2-.5.1-.6-.1-.1-.4-.2-.8-.4Z"/></svg></span>', color: '#25D366' },
-    { nombre: 'LinkedIn', url: '', icon: '<span class="social-icon linkedin"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M6.94 8.5A1.56 1.56 0 1 0 6.94 5.4a1.56 1.56 0 0 0 0 3.1ZM5.5 9.6h2.9V19H5.5Zm4.8 0h2.8v1.3h.04c.4-.8 1.3-1.6 2.8-1.6 3 0 3.5 2 3.5 4.5V19h-2.9v-8.3c0-2-.1-4.5-2.8-4.5-2.8 0-3.2 2.2-3.2 4.4V19H10.3Z"/></svg></span>', color: '#0A66C2' }
+    { nombre: 'Facebook', url: '', icon: '<span class="social-icon facebook"><svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c4.56-.93 8-4.96 8-9.75z"/></svg></span>', color: '#1877F2' },
+    { nombre: 'Instagram', url: 'instagram.com/conexion.studios', icon: '<span class="social-icon instagram"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M7 2h10a5 5 0 0 1 5 5v10a5 5 0 0 1-5 5H7a5 5 0 0 1-5-5V7a5 5 0 0 1 5-5Zm0 2a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V7a3 3 0 0 0-3-3H7Zm5 3.5A4.5 4.5 0 1 1 7.5 12 4.5 4.5 0 0 1 12 7.5Zm0 2A2.5 2.5 0 1 0 14.5 12 2.5 2.5 0 0 0 12 9.5Zm5.25-3.25a1.25 1.25 0 1 1-1.25 1.25 1.25 1.25 0 0 1 1.25-1.25Z"/></svg></span>', color: '#e1306c' },
+    { nombre: 'Threads', url: 'threads.com/@conexion.studios', icon: '<span class="social-icon threads"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2c2.4 0 4.2 1 5.5 2.8 1.2 1.6 1.8 3.5 1.8 5.7a10.7 10.7 0 0 1-2.2 6.5c-.8 1.1-1.9 2.1-3.2 2.9a8.6 8.6 0 0 1-7.4.2 7.3 7.3 0 0 1-3.2-3 8.7 8.7 0 0 1-1-4.3c0-2.4.9-4.6 2.6-6.2C7.2 3.3 9.3 2.4 12 2Zm0 2.2c-1.8 0-3.4.8-4.6 2.2-1.1 1.3-1.7 2.9-1.7 4.6 0 1.5.5 2.9 1.4 4 1 .1 1.9.1 2.8-.1 1.4-.3 2.6-1 3.5-2 .8-1 .9-2.2.9-3.4 0-.6-.1-1.2-.2-1.8-.2-.7-.4-1.3-.8-1.8-.7-.9-1.7-1.4-2.8-1.6-.7-.1-1.3-.1-2 .1a4.2 4.2 0 0 0-1.7 1.2c-.4.5-.7 1-.7 1.6 0 .6.2 1.2.5 1.7a3 3 0 0 0 2.1 1.3c.8.1 1.6-.1 2.3-.4.4-.2.8-.4 1.1-.8.2-.3.3-.6.3-.9 0-.4-.2-.8-.6-1.1-.6-.4-1.4-.5-2.1-.4-.6.1-1.2.4-1.7.8-.4.4-.7.8-.8 1.3-.2.9.1 1.8.8 2.4.6.5 1.4.8 2.2.8.8 0 1.6-.3 2.2-.8.8-.7 1.2-1.7 1.3-2.8.1-1.4-.2-2.8-.9-4.1-.6-1-1.4-1.7-2.4-2.3-.6-.3-1.3-.5-2-.6-.9-.1-1.8-.1-2.6.1-1.3.3-2.4 1-3.2 2-.5.6-.9 1.3-1 2-.2 1-.1 2 .3 2.9.7 1.5 2 2.5 3.6 2.8.9.2 1.8.2 2.8 0 1.4-.3 2.7-1.2 3.6-2.4.8-1.2 1.1-2.6.9-4-.2-1.4-.6-2.8-1.5-3.9C15 4 13.7 2.8 12 2.2Z"/></svg></span>', color: '#000000' },
+    { nombre: 'Tik Tok', url: 'tiktok.com/@conexionstudiosmx', icon: '<span class="social-icon tiktok"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M14.5 2h2.2a4.8 4.8 0 0 0 4.8 4.8v2.4a7.2 7.2 0 0 1-4.8-1.6v7.1a5.2 5.2 0 1 1-4.9-5.2c.2 0 .4 0 .6.1v2.3a2.9 2.9 0 1 0 2.3 2.8V2Z"/></svg></span>', color: '#000000' },
+    { nombre: 'Spotify', url: '', icon: '<span class="social-icon spotify"><svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2zm4.586 14.424a.62.62 0 0 1-.853.206c-2.347-1.434-5.303-1.76-8.784-.966a.622.622 0 0 1-.277-1.213c3.811-.872 7.076-.496 9.707 1.114a.62.62 0 0 1 .207.859zm1.226-2.719a.777.777 0 0 1-1.067.256c-2.686-1.652-6.784-2.133-9.962-1.17a.778.778 0 0 1-.446-1.492c3.633-1.103 8.148-.567 11.22 1.328a.777.777 0 0 1 .255 1.078zm.106-2.831C14.392 8.813 8.414 8.614 4.965 9.661a.933.933 0 1 1-.54-1.785c3.966-1.204 10.55-1.002 14.58 1.39a.933.933 0 1 1-.96 1.597z"/></svg></span>', color: '#1ed760' },
+    { nombre: 'Youtube (Canal Principal)', url: 'www.youtube.com/@SoyCristianRM', icon: '<span class="social-icon youtube"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M23 7.5a3 3 0 0 0-2.1-2.1C19 5 12 5 12 5s-7 0-8.9.4A3 3 0 0 0 1 7.5 31.4 31.4 0 0 0 .5 12a31.4 31.4 0 0 0 .5 4.5 3 3 0 0 0 2.1 2.1C5 19 12 19 12 19s7 0 8.9-.4a3 3 0 0 0 2.1-2.1A31.4 31.4 0 0 0 23.5 12 31.4 31.4 0 0 0 23 7.5Zm-13 8.3V8.2l6 3.8-6 4.8Z"/></svg></span>', color: '#ff0000' },
+    { nombre: 'Youtube (Canal Secundario)', url: 'www.youtube.com/@ConexionStudios-MX', icon: '<span class="social-icon youtube"><svg viewBox="0 0 24 24" aria-hidden="true"><path d="M23 7.5a3 3 0 0 0-2.1-2.1C19 5 12 5 12 5s-7 0-8.9.4A3 3 0 0 0 1 7.5 31.4 31.4 0 0 0 .5 12a31.4 31.4 0 0 0 .5 4.5 3 3 0 0 0 2.1 2.1C5 19 12 19 12 19s7 0 8.9-.4a3 3 0 0 0 2.1-2.1A31.4 31.4 0 0 0 23.5 12 31.4 31.4 0 0 0 23 7.5Zm-13 8.3V8.2l6 3.8-6 4.8Z"/></svg></span>', color: '#ff0000' },
+    { nombre: 'Youtube Music', url: '', icon: '<span class="social-icon yt-music"><svg viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 17c-3.86 0-7-3.14-7-7s3.14-7 7-7s7 3.14 7 7s-3.14 7-7 7zm0-11.5c-2.48 0-4.5 2.02-4.5 4.5s2.02 4.5 4.5 4.5s4.5-2.02 4.5-4.5s-2.02-4.5-4.5-4.5zm-1.5 7v-5l4 2.5l-4 2.5z"/></svg></span>', color: '#ff0000' }
 ];
 
 const colaboracionesData = [
-    { nombre: 'Conexión', rol: 'Director General', bgImage: "/Imagenes/Creadores/Conexion.png", confirmado: true, estado: 'confirmado' },
-    { nombre: 'Cristian R.M', rol: 'Director Principal', bgImage: "/Imagenes/Creadores/Cristian.png", confirmado: true, estado: 'confirmado' },
-    { nombre: 'IDAVEX MX', rol: 'Creador De Contenido', bgImage: "", confirmado: false, estado: 'proximo' },
-    { nombre: 'Miguel A.G', rol: 'Creador De Contenido', bgImage: "", confirmado: false, estado: 'proximo' }
+    { nombre: 'Conexión', rol: 'Director General', bgImage: "Imagenes/Portada De Creadores/Conexión Studios.png", confirmado: true, estado: 'Verificado' },
+    { nombre: 'Cristian R.M', rol: 'Director Principal', bgImage: "Imagenes/Portada De Creadores/Cristian RM.png", confirmado: true, estado: 'Verificado' },
+    { nombre: 'MX IDAVEX', rol: 'Creador De Contenido', bgImage: "Imagenes/Portada De Creadores/MX IDAVEX.jpg", confirmado: false, estado: 'proximo' },
+    { nombre: 'Miguel A.G', rol: 'Creador De Contenido', bgImage: "Imagenes/Portada De Creadores/MIGUEL AG.jpg", confirmado: false, estado: 'proximo' }
 ];
 
 function formatFecha(fecha) {
@@ -128,7 +127,9 @@ function renderEventos() {
     const cards = container.querySelectorAll('.event-card');
     const dots = dotsContainer.querySelectorAll('.roulette-dot');
 
-    cards.forEach((card, index) => card.style.animationDelay = `${index * 0.06}s`);
+    cards.forEach((card, index) => {
+        card.style.animationDelay = `${index * 0.06}s`;
+    });
 
     const listHeight = container.scrollHeight || container.offsetHeight || 1;
     cards.forEach((card, index) => {
@@ -144,19 +145,29 @@ function renderEventos() {
         if (!total) return;
         const scrollRatio = viewport.scrollTop / (viewport.scrollHeight - viewport.clientHeight || 1);
         const index = Math.min(total - 1, Math.max(0, Math.round(scrollRatio * (total - 1))));
-        dots.forEach((dot, i) => dot.classList.toggle('active', i === index));
+        dots.forEach((dot, dotIndex) => dot.classList.toggle('active', dotIndex === index));
     };
 
     viewport.addEventListener('scroll', () => requestAnimationFrame(updateActiveDot), { passive: true });
 
+    // Hacer dots clicables
     dots.forEach((dot, index) => {
         dot.addEventListener('click', () => {
-            cards[index]?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            const targetCard = cards[index];
+            if (targetCard) {
+                targetCard.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                });
+            }
         });
     });
 
     updateActiveDot();
     startCountdown();
+
+    // Notificaciones
+    setupNotifications();
 }
 
 function startCountdown() {
@@ -172,10 +183,7 @@ function startCountdown() {
         const dist = target - Date.now();
         if (dist < 0) {
             el.classList.add('finished');
-            setTimeout(() => {
-                const next = card.nextElementSibling;
-                if (next) next.scrollIntoView({ behavior: 'smooth', block: 'center' });
-            }, 1200);
+            el.innerHTML = '<span>0</span>d <span>0</span>h <span>0</span>m <span class="seconds">0</span>s';
             return;
         }
 
@@ -229,6 +237,48 @@ function renderColaboraciones() {
             </div>
         `;
     }).join('');
+}
+
+// Notificaciones para eventos
+function setupNotifications() {
+    const notifyBtn = document.createElement('button');
+    notifyBtn.id = 'notify-btn';
+    notifyBtn.className = 'notify-button';
+    notifyBtn.innerHTML = `🛎️ Activar notificaciones`;
+
+    const eventosSection = document.querySelector('#eventos-section') || document.querySelector('section') || document.body;
+    if (eventosSection) {
+        eventosSection.style.position = 'relative';
+        eventosSection.appendChild(notifyBtn);
+    }
+
+    notifyBtn.addEventListener('click', async () => {
+        if (!("Notification" in window)) {
+            alert("Tu navegador no soporta notificaciones.");
+            return;
+        }
+
+        let permission = Notification.permission;
+        if (permission === "granted") {
+            showTestNotification();
+        } else if (permission !== "denied") {
+            permission = await Notification.requestPermission();
+            if (permission === "granted") {
+                showTestNotification();
+            }
+        } else {
+            alert("Las notificaciones están bloqueadas. Actívalas en la configuración de tu navegador.");
+        }
+    });
+}
+
+function showTestNotification() {
+    const notification = new Notification("¡Notificaciones activadas!", {
+        body: "Recibirás alertas para los próximos eventos y lives.",
+        icon: "https://via.placeholder.com/128/ff2a2a/ffffff?text=🎤",
+        tag: "event-notification"
+    });
+    notification.onclick = () => window.focus();
 }
 
 // Inicializar
